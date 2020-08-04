a429bedd873f463a004c9780f3f1a50f1c32daff
Tim Tanner <timtanner@tanner-companies.com>
2020-08-04 17:17:08 -0500

    service worker update

---
+++ public/cacheDB.js
@@ -0,0 +1,49 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(evt) {
+  const db = evt.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(evt) {
+  db = evt.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+}};
+
+request.onerror = function(evt) {
+  console.log("Woops! " + evt.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }};
+}
+
+window.addEventListener("online", checkDatabase);
